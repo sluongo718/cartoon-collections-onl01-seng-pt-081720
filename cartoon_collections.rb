923d17b64bc9a807f64e30bd6d2312d12f0be56e
sam l <sluongo718@yahoo.com>
2020-08-23 23:36:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,10 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
    planeteer_calls = []
    array.collect do |planeteer|
+     planeteer_calls << "#{planeteer.upcase}!"
      
    end
+   planeteer_calls
 end
 
 def long_planeteer_calls# code an argument here
